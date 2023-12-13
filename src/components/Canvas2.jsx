ce5865523c5d1569467c94aa5f861ce688ae3a09
J_ Keyy <ashnazg1212@gmail.com>
2023-12-13 10:17:27 +0000

    feat: Add particle movement and canvas creation
    Add particle movement to Canvas1 and create Canvas2 component

--- src/components/Canvas2.jsx
+++ src/components/Canvas2.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from "react";
 import useCanvas from "./useCanvas";
 let ParticlesArray = [];
 const particleCount = 20;
-export default function Canvas1(props) {
+export default function Canvas2(props) {
   const canvasRef = useCanvas(draw);
   //created the particles
   ParticlesArray = useMemo(() => {
@@ -13,8 +13,6 @@ export default function Canvas1(props) {
         x: Math.random() * 500,
         y: Math.random() * 250,
         radius: Math.floor((Math.random() + 1) * 5),
-        speedX: (Math.random() - 0.5) * 1,
-        speedY: (Math.random() - 0.5) * 1,
       };
       ParticlesArray.push(particle);
     }
@@ -32,39 +30,12 @@ export default function Canvas1(props) {
   );
 }
 const draw = (ctx, frameCount, ratio) => {
-  // console.log(ctx);
   ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
   /// hande the paticles
   ParticlesArray.forEach((particle, i) => {
-    for (let index = i; index < ParticlesArray.length; index++) {
-      if (
-        Math.hypot(
-          particle.x - ParticlesArray[index].x,
-          particle.y - ParticlesArray[index].y,
-        ) < 100
-      ) {
-        ctx.beginPath();
-        ctx.moveTo(particle.x, particle.y);
-        ctx.lineTo(ParticlesArray[index].x, ParticlesArray[index].y);
-        ctx.stroke();
-        ctx.strokeStyle = "#00ff00";
-      }
-    }
     ctx.fillStyle = "#ff0000";
     ctx.beginPath();
-    ctx.arc(
-      (particle.x += particle.speedX),
-      (particle.y += particle.speedY),
-      particle.radius,
-      0,
-      2 * Math.PI,
-    );
+    ctx.arc(particle.x, particle.y, particle.radius, 0, 2 * Math.PI);
     ctx.fill();
-    particle.x < 0 || particle.x > ctx.canvas.width
-      ? (particle.speedX *= -1)
-      : null;
-    particle.y < 0 || particle.y > ctx.canvas.height
-      ? (particle.speedY *= -1)
-      : null;
   });
 };
--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,9 +1,12 @@
 import Canvas1 from "./Canvas1";
+import Canvas2 from "./Canvas2";
 export default function World(props) {
   console.log("rendered");
   return (
     <>
       <Canvas1 />
+      {/*  consolation*/}
+      {/* <Canvas2 /> */}
     </>
   );
 }
