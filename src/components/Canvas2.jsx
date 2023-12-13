91d8217b73be97908dc8d2522bce4fe3a343c3e9
J_ Keyy <ashnazg1212@gmail.com>
2023-12-13 15:50:42 +0000

    fix: Fix mouse position and force calculation in Canvas2 component
    Update the Canvas2 component to correctly calculate the mouse position and force for particle movement.

--- src/components/Canvas2.jsx
+++ src/components/Canvas2.jsx
@@ -3,6 +3,7 @@ import useCanvas from "./useCanvas";
 const particleCount = 20;
 export default function Canvas1() {
   const particleArrayRef = useRef([]);
+  const mousePosition = useRef({ x: undefined, y: undefined });
   const canvasRef = useCanvas(draw);
   //created the particles
   particleArrayRef.current = useMemo(() => {
@@ -19,18 +20,35 @@ export default function Canvas1() {
       ParticlesArray.push(particle);
     }
     return ParticlesArray;
-  }, []);
+  }, [particleArrayRef]);
 
   function draw(ctx, frameCount, ratio) {
     // console.log(ctx);
     ctx.clearRect(0, 0, ctx.canvas.width * ratio, ctx.canvas.height * ratio);
+    ///mousePosition
+    // ctx.fillStyle = "#00ff00";
+    // ctx.beginPath();
+    // ctx.arc(
+    //   mousePosition.current.x,
+    //   mousePosition.current.y,
+    //   40,
+    //   0,
+    //   2 * Math.PI,
+    // );
+    // ctx.fill();
     /// hande the paticles
     particleArrayRef.current.forEach((particle, i) => {
+      let force = 0;
+      const dx = mousePosition.current.x - particle.x;
+      const dy = mousePosition.current.y - particle.y;
+      const distance = Math.hypot(dy, dx);
+      const angle = Math.atan2(particle.y, particle.x);
+      distance < 50 ? (force = 3) : null;
       ctx.fillStyle = "#ff0000";
       ctx.beginPath();
       ctx.arc(
-        (particle.x += particle.speedX),
-        (particle.y += particle.speedY),
+        (particle.x += particle.speedX + force),
+        (particle.y += particle.speedY + force),
         particle.radius,
         0,
         2 * Math.PI,
@@ -42,12 +60,17 @@ export default function Canvas1() {
       particle.y < 0 || particle.y > ctx.canvas.height
         ? (particle.speedY *= -1)
         : null;
+
+      // for (let index = i; index < particleArrayRef.current.length; index++) {
     });
   }
-
+  function handleClick(e) {
+    mousePosition.current.x = e.clientX;
+    mousePosition.current.y = e.clientY;
+  }
   return (
     <>
-      <canvas id="canvas1" ref={canvasRef}></canvas>
+      <canvas id="canvas1" ref={canvasRef} onClick={handleClick}></canvas>
     </>
   );
 }
